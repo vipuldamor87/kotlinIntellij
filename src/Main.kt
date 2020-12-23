60c9b6fba0f7fd6e492d1ff3402a498ccdae04cf
vipuldamor87 <vipuldamor26@gmail.com>
2020-12-23 15:40:53 +0530

    commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,31 +1,13 @@
+fun sayHello(greeting:String,itemToGreet:List<String> ) {
+    itemToGreet.forEach{itemToGreet ->
+    println("$greeting $itemToGreet")}
+}
 
-//top level Variables
-var topLevelVar= 12 // Specifying data type not necessary
-var notNullVar: String? = null  //using ? to  to use Null values
 
-fun getGreeting(): String{
-    return "get Greeting Function called"
-}
-fun sayHello(): Unit{
-    // retun type Unit is similar to void in java
-    println("Say hello Function called")
-}
-fun sayHelloWithPara(itemToGreet: String)
-{
-    var msg= "saying hello with $itemToGreet"
-    println(msg)
-}
 
 fun main(){
-    val assignOnce: String ="Vipul"
-    var canBeAssignedMultipleTimes: String = "Damor"
-    println(assignOnce)
-    println(canBeAssignedMultipleTimes)
-    println(topLevelVar)
-
-    //output using Function
-    println(getGreeting())
-    sayHello()
-    sayHelloWithPara("stringPassed")
-
-   }
+    val interestingThings= listOf("Kotlin","programming","comic books")
+    sayHello("hii",interestingThings)
+    //passing param with different order
+    sayHello(itemToGreet = interestingThings,greeting = "hii")
+}
