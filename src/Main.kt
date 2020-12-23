63cb701babcac81f453b743129ff8c81c3e8873a
vipuldamor87 <vipuldamor26@gmail.com>
2020-12-23 14:55:02 +0530

    commit

--- src/Main.kt
+++ src/Main.kt
@@ -3,6 +3,19 @@
 var topLevelVar= 12 // Specifying data type not necessary
 var notNullVar: String? = null  //using ? to  to use Null values
 
+fun getGreeting(): String{
+    return "get Greeting Function called"
+}
+fun sayHello(): Unit{
+    // retun type Unit is similar to void in java
+    println("Say hello Function called")
+}
+fun sayHelloWithPara(itemToGreet: String)
+{
+    var msg= "saying hello with $itemToGreet"
+    println(msg)
+}
+
 fun main(){
     val assignOnce: String ="Vipul"
     var canBeAssignedMultipleTimes: String = "Damor"
@@ -10,16 +23,17 @@ fun main(){
     println(canBeAssignedMultipleTimes)
     println(topLevelVar)
 
-    when(notNullVar){
-        null -> println("Hi it was null")
-        else -> println(notNullVar)
-    }
-    when(topLevelVar) {
-       in  1..3 -> println("It is between 1 and 3")
-       in  4..5 -> println("it is between 4 and 5")
-        in 6..10 -> println("it is between 6 and 12")
-        11,12 -> println("it is 11 or 12")
-        else -> println("not under the range")
-    }
+    //output using Function
+    println(getGreeting())
+    sayHello()
+    sayHelloWithPara("stringPassed")
+
+
+
+
+
+
+
+
 
    }
