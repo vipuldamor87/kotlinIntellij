19de56591be0fd67da30608d39db777c6776d40a
vipuldamor87 <vipuldamor26@gmail.com>
2020-12-23 18:29:53 +0530

    commit

--- src/HigherOrderFunction.kt
+++ src/HigherOrderFunction.kt
@@ -4,14 +4,29 @@ fun printFilteredStrings(list: List<String>, predicate: ((String) -> Boolean)?){
         if(predicate?.invoke(it)== true){
             println(it)
         }
-
     }
-
 }
+
+val predicate: (String) -> Boolean = {
+    it.startsWith("j")
+}
+
+fun getPrintPredicate(): (String) -> Boolean{
+    return { it.startsWith("j")}
+}
+
 fun main() {
     val list = listOf("kotlin", "java", "javascript")
-    printFilteredStrings(list) {
+    printFilteredStrings(list,predicate)
+    printFilteredStrings(list,getPrintPredicate())
+    printFilteredStrings(list,null)
+
+    list
+        .filterNotNull()
+        .filter{
         it.startsWith("j")
+    }.forEach {
+        println(it)
     }
 }
     
