8208573241383729d994d324474872ed05a2f67b
vipuldamor87 <vipuldamor26@gmail.com>
2020-12-23 16:30:58 +0530

    commit

---
+++ src/PersonInfoProvider.kt
@@ -0,0 +1,13 @@
+interface PersonInfoProvider {
+    fun printInfo(person: Person)
+}
+class BasicInfoProvider : PersonInfoProvider {
+    override fun printInfo(person: Person) {
+       println("printInfo")
+    }
+}
+
+fun main(){
+    val provider = BasicInfoProvider()
+    provider.printInfo(Person())
+}
