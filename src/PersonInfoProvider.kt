5077592702e5f2a6b74c555f9e458f1ab5204ae5
vipuldamor87 <vipuldamor26@gmail.com>
2020-12-23 16:52:15 +0530

    commit

--- src/PersonInfoProvider.kt
+++ src/PersonInfoProvider.kt
@@ -1,13 +1,40 @@
 interface PersonInfoProvider {
-    fun printInfo(person: Person)
+    val providerInfo : String
+    fun printInfo(person: Person){
+        println(providerInfo)
+        person.printInfo()
+    }
 }
-class BasicInfoProvider : PersonInfoProvider {
+
+interface  SessionInfoProvider{
+    fun getSessionId() : String
+}
+
+class BasicInfoProvider : PersonInfoProvider,SessionInfoProvider {
+    override val providerInfo: String
+        get() = "BasicInfoProvider"
+
     override fun printInfo(person: Person) {
-       println("printInfo")
+        super.printInfo(person)
+        println("additonal print statement")
+    }
+
+    override fun getSessionId(): String {
+        return "Session"
+    }
+}
+
+fun checkTypes(infoProvider: PersonInfoProvider){
+    if(infoProvider is SessionInfoProvider){
+        println("is a session info provider")
+    } else {
+        println("not a session info provider")
     }
 }
 
 fun main(){
     val provider = BasicInfoProvider()
     provider.printInfo(Person())
+    provider.getSessionId()
+    checkTypes(provider)
 }
