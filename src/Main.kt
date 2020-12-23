eab8dc7a0bb633c9967486ea7d8c70c69d8f8a05
vipuldamor87 <74131418+vipuldamor87@users.noreply.github.com>
2020-12-23 13:25:33 +0530

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,7 @@
 
 //top level Variables
-var topLevelVar= "12" // Specify data type not necessary
+var topLevelVar= 12 // Specifying data type not necessary
+var notNullVar: String? = null  //using ? to  to use Null values
 
 fun main(){
     val assignOnce: String ="Vipul"
@@ -8,5 +9,16 @@ fun main(){
     println(assignOnce)
     println(canBeAssignedMultipleTimes)
     println(topLevelVar)
-    //println("helloworld")
-}
+
+    when(notNullVar){
+        null -> println("Hi it was null")
+        else -> println(notNullVar)
+    }
+    when(topLevelVar) {
+       in  1..3 -> println("It is between 1 and 3")
+       in  4..5 -> println("it is between 4 and 5")
+        in 6..12 -> println("it is between 6 and 12")
+        else -> println("not under the range")
+    }
+
+   }
