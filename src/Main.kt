6fc11fed65260aaef4b13fe17198c619863d3e1f
vipuldamor87 <vipuldamor26@gmail.com>
2020-12-24 15:48:31 +0530

    commit

--- src/Main.kt
+++ src/Main.kt
@@ -9,9 +9,18 @@ fun main(){
 
     var a3 = a1.plus(a2)
     a3.show()
+    var a4 = a1 plus2 a2
+    a4.show()
 }
 
-fun Alien.plus(a : Alien) :Alien
+fun Alien.plus(a : Alien) : Alien
+{
+    var newAlien = Alien()
+    newAlien.skills = this.skills + " " + a.skills
+    return newAlien
+}
+
+infix fun  Alien.plus2(a: Alien) : Alien
 {
     var newAlien = Alien()
     newAlien.skills = this.skills + " " + a.skills
