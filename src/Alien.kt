11c77d522d1e27992a921555927296280b55d9eb
vipuldamor87 <vipuldamor26@gmail.com>
2020-12-24 15:43:33 +0530

    commit

---
+++ src/Alien.kt
@@ -0,0 +1,9 @@
+class Alien {
+    var skills : String? = null
+
+    fun show()
+    {
+        println(skills)
+    }
+
+}
--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,20 @@
 fun main(){
-    println("hello world")
+    var a1 = Alien()
+    a1.skills = "java"
+    a1.show()
+
+    var a2 = Alien()
+    a2.skills = "sql"
+    a2.show()
+
+    var a3 = a1.plus(a2)
+    a3.show()
 }
+
+fun Alien.plus(a : Alien) :Alien
+{
+    var newAlien = Alien()
+    newAlien.skills = this.skills + " " + a.skills
+    return newAlien
+}
+
