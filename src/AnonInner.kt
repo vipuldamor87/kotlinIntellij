fef924ded948af17552031f7c38215dd6301da35
vipuldamor87 <vipuldamor26@gmail.com>
2020-12-24 16:53:12 +0530

    commit

---
+++ src/AnonInner.kt
@@ -0,0 +1,13 @@
+interface  Human
+{
+    fun think()
+}
+fun main(){
+    var programmer : Human = object :Human
+                        {
+                            override fun think() {
+                                println("Think Virtually..")
+                            }
+                        }
+    programmer.think()
+}
